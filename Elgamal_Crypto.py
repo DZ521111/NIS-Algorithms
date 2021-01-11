9e6f8baccbf2f01beffff1e4ef2ddcbf77d4f6cc
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-11 15:41:59 +0530

    Elgamal.py code added!

--- Elgamal_Crypto.py
+++ Elgamal_Crypto.py
@@ -6,6 +6,18 @@ Author : Dhruv B Kakadiya
 from prime_number_generater import simple_testing, miller_rabin_test
 # In python there is a module called Elgamal!
 
+# to find multiplicative_inverse
+def find_mul_inverse (a, n):
+    mod = n
+    t1, t2 = 0, 1
+    while(a > 0):
+        q = n // a
+        r = n - (q * a)
+        n, a = a, r
+        t = t1 - (q * t2)
+        t1, t2 = t2, t
+    return (t1 % mod)
+
 
 if __name__ == "__main__":
     n = int(input("\nEnter the number of bits of prime number :- "))
