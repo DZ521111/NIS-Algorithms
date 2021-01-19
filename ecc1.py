083783b2d15ae1b0d2c97546abe712ad83f8897c
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-19 18:30:13 +0530

    ecc1 algorithm updated!

--- ecc1.py
+++ ecc1.py
@@ -4,6 +4,7 @@ Author : Dhruv B Kakadiya
 '''
 # import libraries
 from multiplicative_inverse import find_mul_inverse
+from prime_number_generater import simple_testing, miller_rabin_test
 
 # find power mod prime
 def power_nmod (a, n, prime):
@@ -18,3 +19,14 @@ def power_nmod (a, n, prime):
         a = (a ** 2) % prime
     return (res)
 
+# main if condition
+if __name__ == "__main__":
+    n = int(input("\nEnter the number of bits of prime number :- "))
+    while (True):
+        n_bit_prime = simple_testing(n)
+        if (not miller_rabin_test(n_bit_prime)):
+            continue
+        else:
+            prime = n_bit_prime
+            break
+    print(f"\nThe prime number is => '{prime}'")
